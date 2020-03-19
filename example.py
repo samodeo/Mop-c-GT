69a3094600d4552a8f5be44eb7873d314cad8067
samodeo <sa649@cornell.edu>
2020-03-19 10:15:29 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from .gnfw import rho_gnfw1h, Pth_gnfw1h
+from .obb import rho, Pth
 import mopc as mop
 
 '''set halo redshift and mass [Msun]
@@ -24,7 +26,7 @@ rho0 = 4e3 * (m/1e14)**0.29 * (1+z)**(-0.66)
 xc = 0.5
 bt = 3.83 * (m/1e14)**0.04 * (1+z)**(-0.025)
 par_rho = [rho0,xc,bt]
-rho_gnfw = mop.rho_gnfw1h(x,par_rho)
+rho_gnfw = rho_gnfw1h(x,m,z,par_rho)
 temp_ksz_gnfw = mop.make_a_obs_profile_sim_rho(theta,m,z,par_rho)
 
 plt.figure(0)
@@ -48,7 +50,7 @@ P0 = 18.1 * (m/1e14)**0.154 * (1+z)**(-0.758)
 al = 1.0
 bt = 4.35 * (m/1e14)**0.0393 * (1+z)**0.415
 par_pth = [P0, al,bt]
-pth_gnfw = mop.Pth_gnfw1h(x,par_pth,m)
+pth_gnfw = Pth_gnfw1h(x,m,z,par_pth)
 temp_tsz_gnfw = mop.make_a_obs_profile_sim_pth(theta,m,z,par_pth)
 
 plt.figure(1)
@@ -72,8 +74,8 @@ alpha_NT = 0.13 #non-thermal pressure norm.
 eff = 2e-5 #feedback efficiency
 par_obb = [gamma,alpha_NT,eff] 
 par2_obb = mop.find_params_M(M,par_obb) #P_0, rho_0, x_f
-rho_obb = mop.rho(x,m,par_obb,par2_obb) 
-pth_obb = mop.Pth(x,m,par_obb,par2_obb)
+rho_obb = rho(x,m,z,par_obb,par2_obb) 
+pth_obb = Pth(x,m,z,par_obb,par2_obb)
 temp_ksz_obb = mop.make_a_obs_profile(theta,m,z,par_obb)[0]
 temp_tsz_obb = mop.make_a_obs_profile(theta,m,z,par_obb)[1]
 
