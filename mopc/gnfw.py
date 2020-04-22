e24ad464fc4fd5201ff4f3560c98d47b5b573153
samodeo <sa649@cornell.edu>
2020-04-22 13:17:10 -0400

    Update gnfw.py

--- mopc/gnfw.py
+++ mopc/gnfw.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .params import cosmo_params
 from .cosmo import rho_cz
+from scipy.interpolate import Rbf
 
 Msol_cgs = 1.989e33
 rhocrit =  1.87847e-29 * cosmo_params['hh']**2
@@ -20,29 +21,55 @@ def r200(M, z):
     return ans
 
 
-def rho_gnfw1h(xx, M, z, theta):
+def rho_gnfw1h(x, M, z, theta):
     '''generalized NFW profile describing the gas density [g/cm3],
     parameters have mass and redshift dependence, as in Battaglia 2016
     '''
-    #rho0 = 4e3 * (M/1e14)**0.29 * (1+z)**(-0.66)
-    #xc = 0.5
-    al = 0.88 * (M/1e14)**(-0.03) * (1+z)**0.19
-    #bt = 3.83 * (M/1e14)**0.04 * (1+z)**(-0.025)
-    gm = -0.2
-    rho0,xc,bt = theta
-    #rho0,al,bt = theta
-    ans = 10**rho0 * (xx/xc)**gm / ((1 + (xx/xc)**al)**((bt-gm)/al))
-    ans *= rho_cz(z) * fb
-    return ans
+    if isinstance(M, float) is True:
+        r200c = r200(M,z)
+        rvir = r200(M,z)/kpc_cgs/1e3 #Mpc
+        xx = x/rvir
+        #rho0 = 4e3 * (M/1e14)**0.29 * (1+z)**(-0.66)
+        #xc = 0.5
+        al = 0.88 * (M/1e14)**(-0.03) * (1+z)**0.19
+        #bt = 3.83 * (M/1e14)**0.04 * (1+z)**(-0.025)
+        gm = -0.2
+        rho0,xc,bt = theta
+        #rho0,al,bt = theta
+        ans = 10**rho0 * (xx/xc)**gm / ((1 + (xx/xc)**al)**((bt-gm)/al))
+        ans *= rho_cz(z) * fb
+        return ans
+    else:
+    #logm = np.log10(M)
+    #h,b_edges = np.histogram(logm, bins=9)
+    #b_cen = np.array([(b_edges[i]+b_edges[i-1])*0.5 for i in range(1,len(b_edges))])
+    #b_len = np.array([(b_edges[i]-b_edges[i-1]) for i in range(1,len(b_edges))])
+    #integ = np.sum(10**b_len*h)
+    #p = h/integ
+        b_cen = np.array([[11.50192134, 11.97728473, 12.45264813, 12.92801152, 13.40337492, 13.87873831,14.35410171, 14.8294651,  15.3048285 ]]).T
+        p = np.array([1.04087299e-05, 1.01113376e-04, 3.47012185e-02, 1.54896770e-01,1.08353391e-01, 3.04157957e-02, 5.58502707e-03, 5.96271527e-04,2.52783440e-05])
+        rho = []
+        for i in range(0, len(b_cen)):
+            m = 10**b_cen[i]
+            r200c = r200(m,z)
+            rvir=r200(m,z)/kpc_cgs/1e3 #Mpc
+            al = 0.88 * (m/1e14)**(-0.03) * (1+z)**0.19
+            gm = -0.2
+            rho0,xc,bt = theta
+            xx = x/rvir
+            rho.append(10**rho0 * (xx/xc)**gm / ((1 + (xx/xc)**al)**((bt-gm)/al))*rho_cz(z) * fb)   
+        rho = np.array(rho)
+        rho_av = np.average(rho, weights=p, axis=0)
+        return rho_av
 
-def rho_gnfw2h(xx,z,theta2h):
+def rho_gnfw2h_old(xx,z,theta2h):
     zbin = [0.45, 0.48, 0.52, 0.56, 0.6, 0.64, 0.68]
     x1 = np.logspace(np.log10(0.1),np.log10(10),50)
     rho2h = []
     for ix in range(0,len(x1)):
         rho2h_z = []
         for iz in zbin:
-            rho = np.genfromtxt('/Users/samodeo/Desktop/2halo/zbin/data/rhoGNFW_M2e+13_z'+str(iz)+'.txt')        
+            rho = np.genfromtxt('data/rhoGNFW_M2e+13_z'+str(iz)+'.txt')        
             rho2h_z.append(rho[ix,2])
         rho2h_z = np.array(rho2h_z)
         rho2h.append(np.interp(z,zbin,rho2h_z))
@@ -50,10 +77,16 @@ def rho_gnfw2h(xx,z,theta2h):
     ans = np.interp(xx,x1,rho2h)
     return theta2h * ans
 
-def rho_gnfw(xx,M,z,theta):
+
+def rho_gnfw2h(xx,zz,theta2h, rho2h):
+    zout= xx * 0.0 + zz
+    return theta2h * rho2h(xx,zout)
+
+
+def rho_gnfw(xx,M,z,theta, rho2h):
     theta1h = theta[0], theta[1], theta[2]
     theta2h = theta[3]
-    ans = rho_gnfw1h(xx,M,z,theta1h) + rho_gnfw2h(xx,z,theta2h)
+    ans = rho_gnfw1h(xx,M,z,theta1h) + rho_gnfw2h(xx,z,theta2h,rho2h)
     return ans
 
 
@@ -61,6 +94,7 @@ def Pth_gnfw1h(x,M,z,theta):
     '''generalized NFW profile describing the thermal pressure [cgs units],
     parameters have mass and redshift dependence, as in Battaglia et al. (2012)
     '''
+    
     if isinstance(M, float) is True:
         r200c = r200(M,z)
         rvir=r200(M,z)/kpc_cgs/1e3 #Mpc
@@ -77,13 +111,15 @@ def Pth_gnfw1h(x,M,z,theta):
         ans = P0 * (x/xc)**gm * (1+(x/xc)**al)**(-bt)
         ans *= P200c
         return ans
-    else:
-        logm = np.log10(M)
-        h,b_edges = np.histogram(logm, bins=49)
-        b_cen = np.array([(b_edges[i]+b_edges[i-1])*0.5 for i in range(1,len(b_edges))])
-        b_len = np.array([(b_edges[i]-b_edges[i-1]) for i in range(1,len(b_edges))])
-        integ = np.sum(10**b_len*h)
-        p = h/integ
+    else:    
+        #logm = np.log10(M)
+        #h,b_edges = np.histogram(logm, bins=9)
+        #b_cen = np.array([(b_edges[i]+b_edges[i-1])*0.5 for i in range(1,len(b_edges))])
+        #b_len = np.array([(b_edges[i]-b_edges[i-1]) for i in range(1,len(b_edges))])
+        #integ = np.sum(10**b_len*h)
+        #p = h/integ
+        b_cen = np.array([[11.50192134, 11.97728473, 12.45264813, 12.92801152, 13.40337492, 13.87873831,14.35410171, 14.8294651,  15.3048285 ]]).T
+        p = np.array([1.04087299e-05, 1.01113376e-04, 3.47012185e-02, 1.54896770e-01,1.08353391e-01, 3.04157957e-02, 5.58502707e-03, 5.96271527e-04,2.52783440e-05])
         pth = []
         for i in range(0, len(b_cen)):
             m = 10**b_cen[i]
@@ -94,8 +130,8 @@ def Pth_gnfw1h(x,M,z,theta):
             P0, al, bt = theta
             xc = 0.497 * (m/1e14)**(-0.00865) * (1+z)**0.731
             gm = -0.3
-            xx = x/rvir
-            pth.append(P0 * (xx/xc)**gm * (1+(xx/xc)**al)**(-bt) * P200c)   
+            #xx = x/rvir
+            pth.append(P0 * (x/rvir/xc)**gm * (1+(x/rvir/xc)**al)**(-bt) * P200c)   
         pth = np.array(pth)
         pth_av = np.average(pth, weights=p, axis=0)
         return pth_av
@@ -107,7 +143,7 @@ def Pth_gnfw2h(xx,z,theta2h):
     for ix in range(0,len(x1)):
         pth2h_z = []
         for iz in zbin:
-            pth = np.genfromtxt('/Users/samodeo/Desktop/2halo/zbin/data/PthGNFW_M2e+13_z'+str(iz)+'.txt')        
+            pth = np.genfromtxt('data/PthGNFW_M2e+13_z'+str(iz)+'.txt')        
             pth2h_z.append(pth[ix,2])
         pth2h_z = np.array(pth2h_z)
         pth2h.append(np.interp(z,zbin,pth2h_z))
